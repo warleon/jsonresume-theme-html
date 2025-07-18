746670a71b69cb233b1c1b72cc4e75a2edb8c2de
warleon <51892390+warleon@users.noreply.github.com>
2025-07-17 21:38:54 -0500

    compact

--- assets/compact.js
+++ assets/compact.js
@@ -54,8 +54,8 @@ body {
   background: var(--color-background);
   color: var(--color-primary);
   display: grid;
-  font: 1em/1.4 system-ui, sans-serif;
-  gap: 1.2em;
+  font: 1em/1.3 system-ui, sans-serif;
+  gap: 0.8em;
   grid-template-columns:
     [full-start]
     1fr
@@ -67,7 +67,7 @@ body {
     1fr
     [full-end];
   grid-template-rows: auto [content] 0;
-  margin-bottom: 2em;
+  margin-bottom: 1.5em;
 }
 
 body::before {
@@ -76,14 +76,33 @@ body::before {
   grid-row: content;
 }
 
-/* PROXIMITY PRINCIPLE: Group related list items closer */
+/* HORIZONTAL-FIRST LISTS: Minimize vertical space usage */
 ol,
 ul {
-  padding-left: 1em;
+  padding-left: 0.8em;
 }
 
-:not(.icon-list, .tag-list) > li + li {
-  margin-top: 0.25em;
+/* Reduce vertical spacing for regular lists */
+:not(.icon-list, .tag-list, .grid-list) > li + li {
+  margin-top: 0.15em;
+}
+
+/* Make regular lists more horizontal when content allows */
+:not(.icon-list, .tag-list, .grid-list) > li {
+  display: inline-block;
+  margin-right: 1.5em;
+  vertical-align: top;
+  width: auto;
+  min-width: 12em;
+}
+
+/* Override inline for longer content */
+:not(.icon-list, .tag-list, .grid-list) > li:has(> p),
+:not(.icon-list, .tag-list, .grid-list) > li:has(> div),
+:not(.icon-list, .tag-list, .grid-list) > li:has(> article) {
+  display: block;
+  margin-right: 0;
+  width: 100%;
 }
 
 li::marker,
@@ -116,13 +135,13 @@ h3 {
 
 h1 {
   font-size: var(--scale5);
-  margin-bottom: 0.3em;
+  margin-bottom: 0.2em;
 }
 
 h2 {
   color: var(--color-secondary);
   font-size: var(--scale3);
-  margin-bottom: 0.5em;
+  margin-bottom: 0.3em;
 }
 
 /* PROXIMITY: Section headings closer to content */
@@ -130,18 +149,18 @@ h3 {
   color: var(--color-secondary);
   font-size: var(--scale2);
   grid-column: side;
-  margin-bottom: 0.5rem;
-  margin-top: 0.3rem;
+  margin-bottom: 0.3rem;
+  margin-top: 0.2rem;
 }
 
 h4 {
   font-size: var(--scale1);
-  margin-bottom: 0.25em;
+  margin-bottom: 0.15em;
 }
 
 h5 {
   font-size: var(--scale0);
-  margin-bottom: 0.2em;
+  margin-bottom: 0.1em;
 }
 
 h6 {
@@ -167,14 +186,14 @@ svg {
   vertical-align: text-bottom;
 }
 
-/* COMPACT MASTHEAD: Reduced padding and tighter spacing */
+/* ULTRA-COMPACT MASTHEAD */
 .masthead {
   background: var(--color-dimmed);
   display: inherit;
-  gap: 0.8em;
+  gap: 0.6em;
   grid-column: full;
   grid-template-columns: inherit;
-  padding: 2em 0;
+  padding: 1.5em 0;
   text-align: center;
 }
 
@@ -186,84 +205,86 @@ section {
 .masthead > img {
   border: 3px solid;
   border-radius: 50%;
-  margin: 0 auto 0.5em;
-  max-width: 8em;
+  margin: 0 auto 0.3em;
+  max-width: 7em;
 }
 
-/* PROXIMITY: Tighter spacing between related content */
+/* MINIMAL VERTICAL SPACING */
 article > * + *,
 blockquote > * + *,
 .timeline > div > * + * {
-  margin-top: 0.4em;
+  margin-top: 0.25em;
 }
 
 .meta {
   color: var(--color-secondary);
-  font-size: 0.9em;
-  margin-top: 0.2em;
+  font-size: 0.85em;
+  margin-top: 0.1em;
 }
 
-/* COMPACT SECTIONS: Reduced gap between sections */
+/* ULTRA-COMPACT SECTIONS */
 .stack {
   display: grid;
-  gap: 1em;
+  gap: 0.6em;
 }
 
 .icon-list {
   display: flex;
   flex-wrap: wrap;
-  gap: 0.3em 0.8em;
+  gap: 0.2em 0.6em;
   justify-content: center;
-  list-style: none;
-  padding: 0;
-  margin-top: 0.5em;
-}
-
-/* PROXIMITY: Skills grouped more tightly */
-.grid-list {
-  display: grid;
-  gap: 0.8em;
-}
-
-.tag-list {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 0.25em 0.3em;
   list-style: none;
   padding: 0;
   margin-top: 0.3em;
 }
 
+/* AGGRESSIVE HORIZONTAL GRID: More columns, tighter spacing */
+.grid-list {
+  display: grid;
+  gap: 0.4em 0.6em;
+  grid-template-columns: repeat(auto-fit, minmax(8em, 1fr));
+}
+
+/* HORIZONTAL-FIRST TAG LIST */
+.tag-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.15em 0.25em;
+  list-style: none;
+  padding: 0;
+  margin-top: 0.2em;
+}
+
 .tag-list > li {
   background: var(--color-dimmed);
   border-radius: 0.2em;
-  padding: 0.15em 0.4em;
-  font-size: 0.9em;
+  padding: 0.1em 0.3em;
+  font-size: 0.85em;
 }
 
-/* COMPACT TIMELINE: Reduced spacing, closer proximity */
+/* ULTRA-COMPACT TIMELINE */
 .timeline > div {
   position: relative;
 }
 
 .timeline > div:not(:last-child) {
-  padding-bottom: 0.6rem;
+  padding-bottom: 0.4rem;
 }
 
 .timeline > div:not(:last-child)::before {
   content: '';
   position: absolute;
-  top: 0.8rem;
+  top: 0.6rem;
   left: -12px;
   width: 1px;
-  height: calc(100% - 0.2rem);
+  height: calc(100% - 0.1rem);
   background: var(--color-secondary);
 }
 
 .timeline > div:not(:only-child)::after {
   content: '';
   position: absolute;
-  top: 0.5rem;
+  top: 0.4rem;
   left: -15px;
   width: 6px;
   height: 6px;
@@ -272,17 +293,17 @@ blockquote > * + *,
   border-radius: 50%;
 }
 
-/* RESPONSIVE: Maintain compact spacing on larger screens */
+/* RESPONSIVE: Maximum horizontal utilization */
 @media print, (min-width: 48em) {
   h3 {
     text-align: right;
     margin-bottom: inherit;
-    margin-top: 0.5rem;
+    margin-top: 0.3rem;
   }
 
   .masthead {
     text-align: inherit;
-    gap: 1em;
+    gap: 0.8em;
   }
 
   .masthead > *,
@@ -302,70 +323,113 @@ blockquote > * + *,
   }
 
   .icon-list {
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 0.2em;
+    justify-content: flex-start;
+    gap: 0.15em 0.8em;
   }
 
-  /* PROXIMITY: Two-column layout for better space usage */
+  /* MAXIMUM HORIZONTAL USAGE: More aggressive columns */
   .grid-list {
-    grid-template-columns: repeat(auto-fit, minmax(calc((100% - 0.8em) / 2), 1fr));
-    gap: 0.6em 0.8em;
+    grid-template-columns: repeat(auto-fit, minmax(6em, 1fr));
+    gap: 0.3em 0.5em;
   }
 
-  /* COMPACT SECTIONS: Sections closer together */
+  /* HORIZONTAL LIST LAYOUT FOR LARGER SCREENS */
+  :not(.icon-list, .tag-list, .grid-list) > li {
+    min-width: 15em;
+    margin-right: 2em;
+  }
+
+  /* TIGHTER SECTIONS */
   body {
-    gap: 0.8em;
+    gap: 0.6em;
+    line-height: 1.25;
   }
 
   .stack {
-    gap: 0.8em;
+    gap: 0.5em;
   }
 }
 
-/* PRINT: Extra compact for print media */
+/* ULTRA-COMPACT PRINT */
 @media print {
   html {
-    font-size: 12px;
+    font-size: 11px;
   }
 
   body {
-    gap: 0.6em;
-    margin-bottom: 1em;
+    gap: 0.4em;
+    margin-bottom: 0.8em;
+    line-height: 1.2;
   }
 
   .masthead {
-    padding: 1.5em 0;
+    padding: 1em 0;
   }
 
   .stack {
-    gap: 0.6em;
+    gap: 0.4em;
   }
 
   .timeline > div:not(:last-child) {
-    padding-bottom: 0.4rem;
+    padding-bottom: 0.3rem;
   }
 
   .duration {
     display: none;
   }
+
+  /* PRINT: Even more aggressive horizontal layouts */
+  .grid-list {
+    grid-template-columns: repeat(auto-fit, minmax(5em, 1fr));
+    gap: 0.2em 0.3em;
+  }
+
+  :not(.icon-list, .tag-list, .grid-list) > li {
+    min-width: 10em;
+    margin-right: 1.5em;
+  }
 }
 
-/* HIERARCHY: Clear visual separation between major sections */
+/* MINIMAL SECTION SEPARATION */
 section + section {
-  margin-top: 0.8em;
+  margin-top: 0.6em;
 }
 
-/* PROXIMITY: Group article headers with their content */
+/* ULTRA-TIGHT ARTICLE HEADERS */
 article header {
-  margin-bottom: 0.3em;
+  margin-bottom: 0.2em;
 }
 
 article header h4 {
-  margin-bottom: 0.1em;
+  margin-bottom: 0.05em;
 }
 
 article header .meta {
-  margin-top: 0.1em;
+  margin-top: 0.05em;
+}
+
+/* HORIZONTAL-FIRST APPROACH FOR WIDE CONTENT */
+@media (min-width: 60em) {
+  body {
+    grid-template-columns:
+      [full-start]
+      1fr
+      [main-start side-start]
+      minmax(min-content, 12em)
+      [side-end content-start]
+      minmax(min-content, 45em)
+      [main-end content-end]
+      1fr
+      [full-end];
+  }
+
+  .grid-list {
+    grid-template-columns: repeat(auto-fit, minmax(5em, 1fr));
+  }
+
+  :not(.icon-list, .tag-list, .grid-list) > li {
+    min-width: 18em;
+    margin-right: 2.5em;
+  }
 }
 `
